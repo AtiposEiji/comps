f63d03528e8e384450940c94864ba182b474d8fa
dbrignoli <davide.brignoli@yourbiz.it>
2023-08-01 16:41:14 +0200

    Move file into pages

---
+++ src/pages/CounterPageConst.js
@@ -0,0 +1,11 @@
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const CHANGE_VALUE_TO_ADD = "change-value-to-add";
+const ADD_VALUE_TO_COUNT = "add-value";
+
+module.exports = {
+    INCREMENT_COUNT,
+    DECREMENT_COUNT,
+    CHANGE_VALUE_TO_ADD,
+    ADD_VALUE_TO_COUNT
+}
